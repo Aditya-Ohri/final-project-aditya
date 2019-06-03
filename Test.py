b81b64989a6272cb9c565a01568a3476a59b9653
AdityaOhri <47825541+AdityaOhri@users.noreply.github.com>
2019-06-03 13:51:31 -0400

    Delete Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-import arcade
-
-def play_sound(file_path: str):
-    sound_effect = arcade.load_sound(file_path)
-    arcade.play_sound(sound_effect)
-
-play_sound("/home/robuntu/Downloads/game_connect_4_playing_disc_place_in_frame_1.wav")
-
-# Winning sound not loading
